2ff44b095cec6ff07c3acd41aa4f7e1c8032d97b
David Saykin <SaykinD@phystech.edu>
2016-03-23 12:36:40 +0300

    Finished integration program.

--- mpi/integration/build.sh
+++ mpi/integration/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mpicc -Wall -g main.c integration.c plot.c -lm -o a.out
-for n in 1 2 3 4 5 6 7 8 9 10;
+mpicc -Wall -g main.c -lm -o a.out
+for n in 1 2 3 4;
 do
-	mpirun -host tor2 -np $n ./a.out $1 > data.dat
 	echo 'Number of processes = '$n
+	mpirun -np $n ./a.out $1 > data.dat
 	cat data.dat
 done
 rm a.out
