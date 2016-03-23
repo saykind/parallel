566c1c2dffd2d97c100c32b956e285776768b537
David Saykin <SaykinD@phystech.edu>
2016-03-23 12:36:40 +0300

    Finished integration program.

--- mpi/examples/sum.c
+++ mpi/examples/sum.c
@@ -1,3 +1,4 @@
+// Sum of integer numbers from 1 to N.
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi/mpi.h>
@@ -5,48 +6,37 @@
 int main(int argc,char *argv[]) {
 	MPI_Init(&argc, &argv);
 	
-	int size, rank, j;
+	int size, rank, j;	double t0;
 	unsigned long int i=0, n=0, m=0, N=0, S = 0, last=0;
-	double start, end;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	//printf("[%d]:\tsize = %d\n", rank, size);
-
-	if (argc < 2) {MPI_Finalize(); return 0;}
+	if (argc < 2) {
+		printf("[%d]:\tHi! I'm #%d. There is %d of us in total.\n", rank, rank, size);
+		MPI_Finalize(); 
+		return 0;
+	}
 	N = atoi(argv[1]); 
+ 	if (!rank) {t0 = MPI_Wtime();}
 
- 	if (rank == 0) {start = MPI_Wtime();}
-	
 	n = N/size;
 	last = (rank+1)*n;
 	if (rank == (size-1)) {last = N+1;}
-
 	for (i = (rank)*n; i < last; i++) {S += i;}
-
-	printf("[%d]:\tsum = %lu\n", rank, S);	
-
+	printf("[%d]:\tsum = %lu\n", rank, S);		
 	
-	
-	if (rank != 0) {
+	if (rank)
 		MPI_Send((void *) &S, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-	}
-	if (rank == 0) {
+	if (!rank) {
 		MPI_Status st;
 		unsigned long int SUM = S;
 		for (j =1 ; j < size; j++) {
 			MPI_Recv((void *) &m, 1, MPI_INT, j, 1, MPI_COMM_WORLD, &st);
-			printf("[%d]:\tRecived from %d: %lu\n", rank,j, m);
+			printf("[%d]:\tRecived from %d: %lu\n", rank, j, m);
 			SUM += m; 
 		}
 		printf("\n[%d]:\tSUM = %lu\n", rank, SUM);
-
-		end = MPI_Wtime();
-		usleep(1000);
-		printf("[%d]:\ttime = %lf millisec\n\n", rank, 1e3*(end-start));
-		
+		printf("[%d]:\ttime = %lf millisec\n\n", rank, 1e3*(MPI_Wtime()-t0));
 	}
- 
-	
 
 	MPI_Finalize();
 	return 0;
--- mpi/integration/main.c
+++ mpi/integration/main.c
@@ -2,76 +2,76 @@
 #include <stdlib.h>
 #include <math.h>
 #include <mpi.h>
-#include "integration.h"
-#include "plot.h"
 #define pi M_PI
+#define abs(x) ((x) > 0) ? (x) : (-(x))
 
 #define a 0.
 #define b 1.
 //double f(double x) {return (x==0.) ? 1 :(sin(x)/x)*(sin(x)/x);}
 //double f(double x) {return (1.0/sqrt(pi))*exp(-x*x/4096);}
-double f(double x) {return (4.0)/(1+x*x);}
+double f(double x) {return (2.0)/(1+x*x);}
 
 
 int main(int argc, char *argv[]) {
 	MPI_Init(&argc, &argv);
-	int size, rank, i, n, N;
-	double S = 0.;
-	double A[2];
+	int size, rank, i, j, n, N;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	if (argc > 2) {sscanf(argv[1],"%d %d", &n, &N);} else {n = 256; N = 4096;}
+	if (argc > 2) {sscanf(argv[1],"%d %d", &n, &N);} else {n = 64; N = 65536;}
 	//printf("Hi I'm #%d. There is %d of us in total.\n", rank, size);
 	double t0 = .0;
 	if (!rank) {t0 = MPI_Wtime();}
+	
+	double h = (b-a)/N;
+	double S = 0.;
 	if (size == 1) {
-		double c[2];
-		c[0] = a;
-		c[1] = b;
-		printf("#%d:\tresult=%.10lf.\n", rank, integrate(f, c, N));
-		printf("#%d:\ttime = %f\n", rank, MPI_Wtime()-t0);
+		S = 0.;
+		for (i = 1; i < N; i+=2) 
+			S += (h/3)*(f(a+h*(i-1))+4*f(a+h*i)+f(a+h*(i+1)));
+		printf("#%d:\tresult =\t%.16lf\n\terror = \t%.16f\n\ttime =\t\t%f sec\n", rank, S, abs(S-pi), MPI_Wtime()-t0);
 		MPI_Finalize();
 		return 0;
 	}
 
 	MPI_Status st;
+	st.MPI_TAG = 1;
+	int M = N/n;
 	double H = (b-a)/n;
-	double I[2];
-	I[1] = a;
+	double A = a;
+	double s = 0.;
 	if (!rank) {
-		for (i = 1; i < size; i++) {
-			I[0] = I[1];
-			I[1] = I[1] + H;
-			MPI_Send((void *) &I, 2, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
+		for (j = 1; j < size; j++) {
+			MPI_Send((void *) &A, 1, MPI_DOUBLE, j, 1, MPI_COMM_WORLD);
+			A += H;
 		}
 	}
-
 	if (rank) {
-		while (I[1] < b) {
-			MPI_Recv((void *) &I, 2, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &st);
-			if (I[0] == b) {continue;}
-			A[0] = integrate(f,I,N/n);
-			A[1] = (double) rank;
-			MPI_Send((void *) &A, 2, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
-//			printf("#%d:\tintegrated on [%f,%f] = %.10lf\n", rank, I[0], I[1], A[0]);
+		MPI_Recv((void *) &A, 1, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &st);
+		while (st.MPI_TAG) {
+			s = 0.;
+			for (i = 1; i < M; i+=2) 
+				s += (h/3)*(f(A+h*(i-1))+4*f(A+h*i)+f(A+h*(i+1)));
+			//for (i = 0; i < M; i++) 
+			//	s += .5*h*(f(A+h*i)+f(A+h*i+h));
+			MPI_Send((void *) &s, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+			MPI_Recv((void *) &A, 1, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &st);
 		} 
 	}
 	if (!rank) {
-		while (I[1] != b) {
-			MPI_Recv((void *) &A, 2, MPI_DOUBLE, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &st);
-			S += A[0];
-			I[0] = I[1];
-			I[1] = I[1] + H;
-			MPI_Send((void *) &I, 2, MPI_DOUBLE,(int) A[1], 1, MPI_COMM_WORLD);
+		while (j < n+1) {
+			MPI_Recv((void *) &s, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &st);
+			S += s;
+			MPI_Send((void *) &A, 1, MPI_DOUBLE, st.MPI_SOURCE, 1, MPI_COMM_WORLD);
+			A += H;
+			j++;
 		}
-		I[0] = I[1];
 		for (i = 1; i < size; i++) {
-			if (i == A[1]) continue;
-			MPI_Send((void *) &I, 2, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
+			MPI_Recv((void *) &s, 1, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, &st);
+			S += s;
+			MPI_Send((void *) &A, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
 		}
 	}
-	if (!rank) printf("#%d:\tresult=%.10lf\n", rank, S);
-	if (!rank) printf("#%d:\ttime = %f\n", rank, MPI_Wtime()-t0);
+	if (!rank) printf("#%d:\tresult =\t%.16lf\n\terror = \t%.16f\n\ttime =\t\t%f sec\n", rank, S, abs(S-pi), MPI_Wtime()-t0);
 	MPI_Finalize();
 	return 0;
 }
